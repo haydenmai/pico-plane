51afe13ec94537e21fde8e6e17c57688ba59ff26
BenHsiang <bdh3@sfu.ca>
2025-05-28 14:15:31 -0700

    Done basic setup on mpu6050 module

---
+++ hal/include/hal/mpu6050.h
@@ -0,0 +1,18 @@
+/**
+ * @file mpu6050.h
+ * @brief Controls the MPU 6050 accelerometer and gyroscope.
+ * @author Benley Hsiang
+ * @date May-28-2025
+ */
+
+#ifndef MPU_6050_H_
+#define MPU_6050_H_
+
+class MPU6050 {
+    // Note to self: Private members' names will have an underscore at the end,
+    // public ones have a double underscore at the end
+  public:
+  private:
+};
+
+#endif
---
+++ hal/src/mpu6050.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file mpu6050.cpp
+ * @brief Controls the MPU 6050 accelerometer and gyroscope.
+ * @author Benley Hsiang
+ * @date May-28-2025
+ */
+
+#include "hal/mpu6050.h"
