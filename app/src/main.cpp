69331a092e5334e6a414ea6fa6b38027f84079bc
BenHsiang <bdh3@sfu.ca>
2025-06-04 14:31:58 -0700

    Got the example code working, added script for printing terminal output

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -32,6 +32,8 @@ int main()
     auto servo_16     = ServoMG90S(16);
     auto servo_17     = ServoMG90S(17);
 
+    sleep_ms(10000);
+
     // Test
 
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN)                          \
@@ -49,44 +51,45 @@ int main()
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN,
                                GPIO_FUNC_I2C));
 
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
-        }
-
-        // Perform a 1-byte dummy read from the probe address. If a slave
-        // acknowledges this address, the function returns the number of bytes
-        // transferred. If the address byte is ignored, the function returns
-        // -1.
-
-        // Skip over any reserved addresses.
-        int ret;
-        uint8_t rxdata;
-        if (reserved_addr(addr))
-            ret = PICO_ERROR_GENERIC;
-        else
-            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
-
-        printf(ret < 0 ? "." : "@");
-        printf(addr % 16 == 15 ? "\n" : "  ");
-    }
-    printf("Done.\n");
-    return 0;
-#endif
-
-    // Test
-
     while (true) {
+
+        printf("\nI2C Bus Scan\n");
+        printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+        for (int addr = 0; addr < (1 << 7); ++addr) {
+            if (addr % 16 == 0) {
+                printf("%02x ", addr);
+            }
+
+            // Perform a 1-byte dummy read from the probe address. If a slave
+            // acknowledges this address, the function returns the number of bytes
+            // transferred. If the address byte is ignored, the function returns
+            // -1.
+
+            // Skip over any reserved addresses.
+            int ret;
+            uint8_t rxdata;
+            if (reserved_addr(addr))
+                ret = PICO_ERROR_GENERIC;
+            else
+                ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+            printf(ret < 0 ? "." : "@");
+            printf(addr % 16 == 15 ? "\n" : "  ");
+        }
+        printf("Done.\n");
+
         onboard_led.on();
-        sleep_ms(100);
-        onboard_led.off();
         sleep_ms(2000);
+        onboard_led.off();
+        sleep_ms(5000);
+
+        // Test
     }
 
     stdio_deinit_all();
     cyw43_arch_deinit();
     return 0;
+
+#endif
 }
