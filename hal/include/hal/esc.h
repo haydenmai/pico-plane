e525c190ee23795151f507acfb0c2104c7437517
BenHsiang <bdh3@sfu.ca>
2025-06-18 16:28:56 -0700

    Added empty esc module

---
+++ hal/include/hal/esc.h
@@ -0,0 +1,22 @@
+/**
+ * @file esc.h
+ * @brief Manages the ESC (Electronic Speed Controller) for an electric motor.
+ * @author Benley Hsiang
+ * @date Jun-18-2025
+ */
+
+#ifndef ESC_H_
+#define ESC_H_
+
+/**
+ * @class
+ * @brief
+ */
+class Esc {
+  public:
+    //
+  private:
+    //
+};
+
+#endif
---
+++ hal/src/esc.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file esc.h
+ * @brief Manages the ESC (Electronic Speed Controller) for an electric motor.
+ * @author Benley Hsiang
+ * @date Jun-18-2025
+ */
+
+#include "hal/esc.h"
