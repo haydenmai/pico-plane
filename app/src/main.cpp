0e621b69daf13e2521a6fd9a9ddc403ace67ccf0
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2025-09-21 16:50:20 -0700

    Removed all commented out code, check commit history for old code

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,12 +6,15 @@
  * @date Aug-22-2025
  */
 
+// hal layer 
 #include "crsf/crsf.h"
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
 
+// app layer
 #include "flight_control.h"
 
+// SDK
 #include "hal/motor_esc.h"
 #include "hal/mpu6050.h"
 #include "hal/pico_led.h"
@@ -19,13 +22,8 @@
 #include "hal/servo_ds_m005.h"
 #include <stdio.h>
 
-// auto esc18 = MotorEsc(18);
 
-auto flightController = FlightController();
-
-// auto led17 = pwmLED(17);
-// auto led18 = pwmLED(18);
-// auto led19 = pwmLED(19);
+static auto flightController = FlightController();
 
 int map_to_range2(int range1_val, int range1_min, int range1_max, int range2_min,
                   int range2_max)
@@ -37,30 +35,7 @@ int map_to_range2(int range1_val, int range1_min, int range1_max, int range2_min
 
 void on_rc_channels(const uint16_t channels[16])
 {
-    /*
-printf("Channel 1: %f\n", TICKS_TO_US(channels[0]));
-printf("Channel 2: %f\n", TICKS_TO_US(channels[1]));
-printf("Channel 3: %f\n", TICKS_TO_US(channels[2]));
-printf("Channel 4: %f\n", TICKS_TO_US(channels[3]));
-printf("Channel 5: %f\n", TICKS_TO_US(channels[4]));
-printf("Channel 6: %f\n", TICKS_TO_US(channels[5]));
-printf("Channel 7: %f\n", TICKS_TO_US(channels[6]));
-printf("Channel 8: %f\n", TICKS_TO_US(channels[7]));
-printf("Channel 9: %f\n", TICKS_TO_US(channels[8]));
-printf("Channel 10: %f\n", TICKS_TO_US(channels[9]));
-printf("Channel 11: %f\n", TICKS_TO_US(channels[10]));
-printf("Channel 12: %f\n", TICKS_TO_US(channels[11]));
-printf("\n");
-// printf("Channel 13: %f\n", TICKS_TO_US(channels[12]));
-// printf("Channel 14: %f\n", TICKS_TO_US(channels[13]));
-// printf("Channel 15: %f\n", TICKS_TO_US(channels[14]));
-// printf("Channel 16: %f\n", TICKS_TO_US(channels[15]));
-    */
-
-    // int deg1 {map_to_range2(TICKS_TO_US(channels[0]), 1000, 2000, 0, 180)};
-    // int deg2 {map_to_range2(TICKS_TO_US(channels[1]), 1000, 2000, 0, 180)};
-    // int deg3 {map_to_range2(TICKS_TO_US(channels[3]), 1000, 2000, 0, 180)};
-
+	// TODO: Do the controls on the other core
     int throttle {map_to_range2(TICKS_TO_US(channels[2]), 1000, 2000, 0, 50)};
 
     int aileron {map_to_range2(TICKS_TO_US(channels[0]), 1000, 2000, 70, 110)};
@@ -72,16 +47,6 @@ printf("\n");
     flightController.changeAngle(AngleController::AILERON, aileron);
     flightController.changeAngle(AngleController::RUDDER, rudder);
     flightController.changeAngle(AngleController::ELEVATOR, elevator);
-
-    // esc18.setSpeed(throttle);
-
-    // srv14.setAngle(deg1);
-    // srv15.setAngle(deg2);
-    // srv16.setAngle(deg3);
-
-    // led17.setBrightness(TICKS_TO_US(channels[2]));
-    // led18.setBrightness(TICKS_TO_US(channels[4]));
-    // led19.setBrightness(TICKS_TO_US(channels[5]));
 }
 
 void on_link_stats(const link_statistics_t link_stats)
@@ -107,30 +72,9 @@ int main()
 
     crsf_begin(uart1, 9, 8);
 
-    for (;;)
+    for (;;) {
         crsf_process_frames();
-    /**
-        sleep_ms(10000);
-
-        servo.setAngle(0);
-
-        MPU6050 joe = MPU6050();
-
-        while (true) {
-
-            MPU6050::AccelVal joemama = joe.getAccelValues();
-            MPU6050::GyroVal amogus   = joe.getGyroValues();
-
-            printf("\nx: %.3fg, y: %.3fg, z: %.3fg\n", joemama.x, joemama.y, joemama.z);
-            printf("\nx: %.3f deg/sec, y: %.3f deg/sec, z: %.3f deg/sec,\n", amogus.x,
-                   amogus.y, amogus.z);
-
-            onboard_led.on();
-            sleep_ms(50);
-            onboard_led.off();
-            sleep_ms(50);
-        }
-        **/
+	}
 
     stdio_deinit_all();
 
