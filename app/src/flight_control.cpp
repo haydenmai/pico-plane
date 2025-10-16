d45cf9c771ea1c6e9b0619d067e14f0d299a498f
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2025-10-15 23:36:26 -0700

    Recalibrated servos for control

--- app/src/flight_control.cpp
+++ app/src/flight_control.cpp
@@ -17,9 +17,9 @@ namespace FlightController {
 
     /** @brief Lower and upper limits of the angles the control surfaces can turn. */
     // TODO: Values are not in range, need to adjust them
-    constexpr AngleController::TurningRange AILERON_RANGE {175, 185};
-    constexpr AngleController::TurningRange RUDDER_RANGE {175, 185};
-    constexpr AngleController::TurningRange ELEVATOR_RANGE {-5, 5};
+    constexpr AngleController::TurningRange AILERON_RANGE {170, 180};
+    constexpr AngleController::TurningRange RUDDER_RANGE {170, 180};
+    constexpr AngleController::TurningRange ELEVATOR_RANGE {0, 10};
     // TODO: Values are not in range, need to adjust them
 
     bool isInitialized_ = false;
