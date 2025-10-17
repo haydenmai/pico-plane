893a0a0c04b50aaa95dada1239441bcde3c55cdb
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2025-10-16 19:59:02 -0700

    Updated ranges

--- app/include/flight_control.h
+++ app/include/flight_control.h
@@ -17,9 +17,9 @@ namespace FlightController {
 
     /** @brief Lower and upper limits of the angles the control surfaces can turn. */
     // TODO: Values are not in range, need to adjust them
-    constexpr AngleController::TurningRange AILERON_RANGE {170, 180};
-    constexpr AngleController::TurningRange RUDDER_RANGE {170, 180};
-    constexpr AngleController::TurningRange ELEVATOR_RANGE {0, 10};
+    constexpr AngleController::TurningRange AILERON_RANGE {150, 180};
+    constexpr AngleController::TurningRange RUDDER_RANGE {150, 180};
+    constexpr AngleController::TurningRange ELEVATOR_RANGE {0, 30};
     // TODO: Values are not in range, need to adjust them
 
     void init();
